1ecaa3703e65d166151acf266f39921b85cd9b8a
Bartek Glowacki <marathone@gmail.com>
2021-12-01 21:41:18 +0100

    Move readings to day_1 dir

--- lib/day_1.rb
+++ lib/day_1.rb
@@ -2,7 +2,7 @@ require_relative "day_1/depth_reading"
 require_relative "day_1/comparator"
 require_relative "day_1/second_comparator"
 
-readings_path = File.join(File.dirname(__FILE__), "readings.txt")
+readings_path = File.join(File.dirname(__FILE__), "day_1/readings.txt")
 
 readings = File.readlines(readings_path).map do |raw_reading|
   DepthReading.new(raw_reading)
