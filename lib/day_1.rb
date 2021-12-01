2a8161eb3bd2d3d461d0e75883b43d83016acf11
Bartek Glowacki <marathone@gmail.com>
2021-12-01 20:31:30 +0100

    Add calculation for grouped readings

--- lib/day_1.rb
+++ lib/day_1.rb
@@ -44,14 +44,54 @@ class Comparator
   end
 end
 
-readings_path = File.join(File.dirname(__FILE__), "readings.txt")
+class GroupedReading
+  def initialize(readings)
+    @readings = readings
+  end
 
+  def sum
+    @readings.inject(0) { |sum, reading| sum + reading.reading }
+  end
+
+  def >(other_reading)
+    sum > other_reading.sum
+  end
+end
+
+class SecondComparator
+  def initialize(readings)
+    @counter = Counter.new
+    @readings = readings
+  end
+
+  def compare
+    group_readings = @readings.each_cons(3).map do |readings|
+      GroupedReading.new(readings)
+    end
+
+
+    group_readings.each_cons(2) do |current_reading, next_reading|
+      @counter.increment if increased?(current_reading, next_reading)
+    end
+
+    @counter
+  end
+
+  private
+
+  def increased?(current_reading, next_reading)
+    next_reading > current_reading
+  end
+end
+
+readings_path = File.join(File.dirname(__FILE__), "readings.txt")
 
 readings = File.readlines(readings_path).map do |raw_reading|
   DepthReading.new(raw_reading)
 end
 
 pp Comparator.new(readings).compare
+pp SecondComparator.new(readings).compare
 
 
 
--- spec/comparator_spec.rb
+++ spec/comparator_spec.rb
@@ -20,3 +20,24 @@ RSpec.describe Comparator do
     expect(counter.count).to eq(7)
   end
 end
+
+RSpec.describe SecondComparator do
+  it 'succeeds' do
+    readings = [
+      199, #  A
+      200, #  A B
+      208, #  A B C
+      210, #    B C D
+      200, #  E   C D
+      207, #  E F   D
+      240, #  E F G
+      269, #    F G H
+      260, #      G H
+      263, #        H
+    ].map { |raw_reading|
+      DepthReading.new(raw_reading)
+    }
+    counter = SecondComparator.new(readings).compare
+    expect(counter.count).to eq(5)
+  end
+end
